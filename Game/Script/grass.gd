e32eeaa4f948b78a68e253a54541cc68eb20f23f
rick <zjw1918@sina.cn>
2025-11-17 22:46:34 +0800

    done - grass interaction - scale when step on

--- Game/Script/grass.gd
+++ Game/Script/grass.gd
@@ -3,6 +3,10 @@ extends Area2D
 var skew_tween: Tween
 var skew_tween_back: Tween
 
+var scale_tween: Tween
+var start_scale = Vector2.ONE
+var end_scale = Vector2(1.0, 0.5)
+
 @onready var sprite_2d_back: Sprite2D = $Sprite2DBack
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
@@ -30,5 +34,20 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	animate_scale(end_scale, 0.1)
+
+func _on_body_exited(_body: Node2D) -> void:
+	animate_scale(start_scale, 0.5)
+
+func animate_scale(pos: Vector2,  duration: float) -> void:
+	if scale_tween:
+		scale_tween.kill()
+	
+	scale_tween = get_tree().create_tween()
+	scale_tween.tween_property(sprite_2d, "scale", pos, duration)
+	scale_tween.set_ease(Tween.EASE_OUT)
